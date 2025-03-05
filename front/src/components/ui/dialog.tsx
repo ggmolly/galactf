4418af377da689d7800ed6c58ba335661e1a241c
ggmolly <molly@molly.sh>
2025-03-05 20:07:36 +0100

    fix: allow to click on a toast from a dialog overlay

--- front/src/components/ui/dialog.tsx
+++ front/src/components/ui/dialog.tsx
@@ -49,10 +49,20 @@ function DialogContent({
   children,
   ...props
 }: React.ComponentProps<typeof DialogPrimitive.Content>) {
+  // Workaround to allow users to click on a 'Retry' button from a toast
+  const handleClickOutside = React.useCallback((e: CustomEvent<{ originalEvent: PointerEvent | FocusEvent }>) => {
+    const toasts = document.querySelectorAll('[data-sonner-toast]');
+    const isInsideToast = Array.from(toasts).some(toast => toast.contains(e.target as HTMLElement));
+    if (isInsideToast) {
+      e.preventDefault();
+    }
+  }, []);
   return (
     <DialogPortal data-slot="dialog-portal">
       <DialogOverlay />
       <DialogPrimitive.Content
+        onPointerDownOutside={handleClickOutside}
+        onInteractOutside={handleClickOutside}
         data-slot="dialog-content"
         className={cn(
           "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
