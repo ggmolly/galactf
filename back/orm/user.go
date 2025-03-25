fa73aeea88b421ee483929e61b83f84c0b60bc47
ggmolly <molly@molly.sh>
2025-03-25 22:24:03 +0100

    refactor: move random seed creation in a function to create used from auth middleware

--- back/orm/user.go
+++ back/orm/user.go
@@ -16,23 +16,30 @@ var (
 type User struct {
 	ID         uint64    `json:"id" gorm:"primaryKey" faker:"-"`
 	Name       string    `json:"name" gorm:"type:varchar(255);unique" faker:"name"`
-	Email      string    `json:"-" gorm:"type:varchar(255);unique,omitempty" faker:"email"`
+	Email      string    `json:"-" gorm:"type:varchar(255);unique,omitempty,index" faker:"email"`
 	CreatedAt  time.Time `json:"-" gorm:"type:timestamp" faker:"-"`
 	RandomSeed uint64    `json:"-" faker:"-" gorm:"type:bigint"`
 
 	Attempts []Attempt `json:"-" gorm:"foreignKey:UserID" faker:"-"`
 }
 
+func GenerateRandomSeed() (uint64, error) {
+	buf := make([]byte, 8)
+	if _, err := cryptoRand.Read(buf); err != nil {
+		return 0, err
+	}
+	return binary.BigEndian.Uint64(buf) % 9223372036854775807, nil
+}
+
 func FakeUsers(n uint) []User {
+	var err error
 	users := make([]User, n)
 	faker.FakeData(&users, options.WithRandomMapAndSliceMinSize(n), options.WithRandomMapAndSliceMaxSize(n))
 
 	for i := range users {
-		buf := make([]byte, 8)
-		if _, err := cryptoRand.Read(buf); err != nil {
+		if users[i].RandomSeed, err = GenerateRandomSeed(); err != nil {
 			panic(err)
 		}
-		users[i].RandomSeed = binary.BigEndian.Uint64(buf) % 9223372036854775807
 	}
 
 	faker.ResetUnique()
