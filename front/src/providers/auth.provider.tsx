de80ffe1e85d2594bb09719d245e16447b09bd08
ggmolly <molly@molly.sh>
2025-03-25 22:34:45 +0100

    feat: implement forest auth & prevent redirect loops

--- front/src/providers/auth.provider.tsx
+++ front/src/providers/auth.provider.tsx
@@ -1,12 +1,7 @@
-import React, {
-  createContext,
-  useContext,
-  useState,
-  useEffect,
-  ReactNode,
-} from "react";
+import React, { createContext, useContext, useState, useEffect, ReactNode } from "react";
 import { User } from "@/interfaces/user.interface";
 import { apiClient } from "@/lib/axios";
+import { toast } from "sonner";
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -18,26 +13,62 @@ interface AuthContextType {
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
+function redirectToForest() {
+  localStorage.setItem("toastOnLogin", "true");
+
+  const redirectCount = Number(localStorage.getItem("redirectCount") || "0");
+  if (redirectCount >= 5) {
+    alert("Redirect loop detected, try clearing your cookies and the local storage.");
+    return;
+  }
+
+  localStorage.setItem("redirectCount", String(redirectCount + 1));
+  window.location.href =
+    "https://forest.galadrim.fr/login?redirect=" +
+    encodeURIComponent(window.location.origin) +
+    "&nonce=" +
+    Math.random();
+}
+
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [user, setUser] = useState<User>({} as User);
+  const [authStatus, setAuthStatus] = useState<"logging-in" | "logged-in" | "logged-out">(
+    "logging-in"
+  );
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const response = await apiClient.get("/auth/me");
-        setUser(response.data.data);
-      } catch (err) {
-        if (!user) {
-          window.location.href = "https://forest.galadrim.fr/login";
+        if (response.status === 200) {
+          setUser(response.data.data);
+          setAuthStatus("logged-in");
+          if (localStorage.getItem("toastOnLogin") === "true") {
+            toast.success(`Logged in as ${response.data.data.name} !`, {
+              dismissible: true,
+              duration: 7000,
+            });
+            localStorage.removeItem("toastOnLogin");
+            localStorage.removeItem("redirectCount");
+          }
+        } else {
+          setAuthStatus("logged-out");
+          redirectToForest();
         }
+      } catch (err) {
+        setAuthStatus("logged-out");
+        redirectToForest();
       }
     };
     fetchUser();
   }, []);
 
-  return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
-  );
+  // Nothing must be rendered unless the user is logged in
+  if (authStatus === "logging-in") {
+    return <div>Logging in...</div>;
+  }
+
+  return <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = (): AuthContextType => {
